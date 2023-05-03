d40e323f41ddabbe5d4b0c0390c5b4d9feb1d146
Kanawat Phuengphadung <capmu@Kanawats-MacBook-Pro.local>
2023-05-03 20:20:57 +0700

    Modify controller to return a map

--- src/main/java/com/capmu/springbootcors/controller/TestController.java
+++ src/main/java/com/capmu/springbootcors/controller/TestController.java
@@ -1,14 +1,20 @@
 package com.capmu.springbootcors.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class TestController {
 
     @GetMapping("/test")
-    public String testApi() {
-        return "This is Capmu";
+    public ResponseEntity<Map<String, String>> testApi() {
+        Map<String, String> sampleResponse = new HashMap<>();
+        sampleResponse.put("message", "Hello, this is Capmu!");
+        return ResponseEntity.ok(sampleResponse);
     }
 
 }
