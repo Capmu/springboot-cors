d3c46c4a530fa9a970a3759207c9979b22ac3451
Kanawat Phuengphadung <capmu@Kanawats-MacBook-Pro.local>
2023-05-03 20:14:20 +0700

    Add CORS configuration to allow all Origins

--- src/main/java/com/capmu/springbootcors/SpringbootCorsApplication.java
+++ src/main/java/com/capmu/springbootcors/SpringbootCorsApplication.java
@@ -1,13 +1,28 @@
 package com.capmu.springbootcors;
 
+import lombok.NonNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringbootCorsApplication {
+
+	@Bean
+	public WebMvcConfigurer cors() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(@NonNull CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*");
+			}
+		};
+	}
 
 	@Bean
 	public SecurityFilterChain chain(HttpSecurity http) throws Exception {
