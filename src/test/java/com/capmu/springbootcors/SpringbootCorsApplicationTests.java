59148c992f7cf5f1b7c09d231d870df3b8c121f9
Kanawat Phuengphadung <capmu@Kanawats-MacBook-Pro.local>
2023-05-03 19:37:46 +0700

    Initiate project

---
+++ src/test/java/com/capmu/springbootcors/SpringbootCorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capmu.springbootcors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
