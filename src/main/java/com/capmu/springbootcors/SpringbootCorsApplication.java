ece7c3a123483dfd8ee70a97aa88267369d1abea
Kanawat Phuengphadung <capmu@Kanawats-MacBook-Pro.local>
2023-05-03 19:46:02 +0700

    Add a test controller and chain bean to let it pass spring security

--- src/main/java/com/capmu/springbootcors/SpringbootCorsApplication.java
+++ src/main/java/com/capmu/springbootcors/SpringbootCorsApplication.java
@@ -2,9 +2,18 @@ package com.capmu.springbootcors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class SpringbootCorsApplication {
+
+	@Bean
+	public SecurityFilterChain chain(HttpSecurity http) throws Exception {
+		http.cors();
+		return http.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootCorsApplication.class, args);
---
+++ src/main/java/com/capmu/springbootcors/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.capmu.springbootcors.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String testApi() {
+        return "This is Capmu";
+    }
+
+}
